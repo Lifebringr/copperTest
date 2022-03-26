520b4462927dab5b7335aed837e5d8107ecc78fe
Lifebringr <jmsutilo@gmail.com>
2022-03-26 19:05:16 +0000

    Update RandomUserRepository.java
    Mobile: updating as we changed the Id to a string

--- src/main/java/co/copper/test/storage/RandomUserRepository.java
+++ src/main/java/co/copper/test/storage/RandomUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RandomUserRepository extends CrudRepository<RandomUser,Long> {
+public interface RandomUserRepository extends CrudRepository<RandomUser,String> {
 
 }
